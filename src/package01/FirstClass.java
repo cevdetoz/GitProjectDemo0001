b52abe6a8c55e2ce5bcba84bd4ad5346001692a1
Maruf Ozkan <74539562+cevdetoz@users.noreply.github.com>
2021-03-13 22:32:48 +0200

    Update FirstClass.java
    changes on remote ibrahim branch

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 //maruf local branch makes chanchs
+		//here is the confligcting line
+		//i will confilict may remote master branch
 	}
 
 }
